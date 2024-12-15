f2f20100c2676255d77b5241d2cb1050482be5b3
Malav Soni <malav@live.com>
2024-12-15 08:45:41 +0530

    BFS and DFS logic added

--- longest_word.ts
+++ longest_word.ts
@@ -1,4 +1,5 @@
-import { Trie } from "./implement_trie";
+import { help } from "yargs";
+import { Trie, TrieNode } from "./implement_trie";
 
 function longestWord(words: string[]): string {
   let result = "";
@@ -11,6 +12,61 @@ function longestWord(words: string[]): string {
       if (words[index].length > result.length) result = words[index];
       set.add(words[index]);
     }
+  }
+
+  return result;
+}
+
+function longestWord_with_trie_dfs(words: string[]): string {
+  let result = "";
+  let trie = new Trie();
+
+  for (let index = 0; index < words.length; index++) {
+    trie.insert(words[index]);
+  }
+
+  function helper(currentWord: string) {
+    if (currentWord.length > result.length) {
+      result = currentWord;
+    }
+    for (let index = 0; index < 26; index++) {
+      let letterA = "a".charCodeAt(0);
+      let charCode = letterA + index;
+      let char = String.fromCharCode(charCode);
+      if (trie.search(currentWord + char)) {
+        helper(currentWord + char);
+      }
+    }
+  }
+
+  helper("");
+
+  return result;
+}
+
+function longestWord_with_trie_bfs(words: string[]): string {
+  let result = "";
+  let trie = new Trie();
+
+  for (let index = 0; index < words.length; index++) {
+    trie.insert(words[index]);
+  }
+
+  let queue: TrieNode[] = [];
+  let wordsQueue: string[] = [];
+  queue.push(trie.root);
+  wordsQueue.push("");
+  while (queue.length != 0) {
+    let current = queue.shift()!;
+    let parentWord = wordsQueue.shift()!;
+    for (let i = 25; i >= 0; i--) {
+      if (current.children[i] != null && current.children[i].is_end) {
+        queue.push(current.children[i]);
+        let char = String.fromCharCode(i + "a".charCodeAt(0));
+        wordsQueue.push(parentWord + char);
+      }
+    }
+    result = parentWord;
   }
 
   return result;
@@ -67,6 +123,36 @@ describe("Longest Word", () => {
       longestWord_with_trie([
         "a",
         "banana",
+        "app",
+        "appl",
+        "ap",
+        "apply",
+        "apple",
+      ])
+    ).toStrictEqual("apple");
+  });
+
+  it("Happy Path - With Trie And DFS", () => {
+    expect(
+      longestWord_with_trie_dfs([
+        "a",
+        "banana",
+        "app",
+        "appl",
+        "ap",
+        "apply",
+        "apple",
+      ])
+    ).toStrictEqual("apple");
+  });
+
+  it("Happy Path - With Trie And BFS", () => {
+    expect(
+      longestWord_with_trie_bfs([
+        "a",
+        "banana",
+        "b",
+        "ba",
         "app",
         "appl",
         "ap",
